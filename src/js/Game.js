c5cb4442ff47fd4398b657dd2685ca5cc2aea312
Meshi Avrahami <meshiavrahami@gmail.com>
2020-09-06 19:27:59 +0300

    added timer

--- src/js/Game.js
+++ src/js/Game.js
@@ -1,16 +1,17 @@
 import Player from './Player.js'; 
 import George from './George.js';
 
+let timer;
+
 export default class Game {
     constructor(){
         this.player = new Player;
-        this.time = 60;
+        this.time = 30;
         this.emenyNumer = 3;
         this.enemies;
         this.gameElement;
-        this.width;
-        this.height;
         this.gameStarted = false;
+        this.gameFinished = false;
         //this.numberOfPlayers // TODO: multiplayer
     }
 
@@ -21,10 +22,31 @@ export default class Game {
             this.enemies = getEnemies(this.emenyNumer);
             this.gameElement = gameElement;
             initBackground(this.gameElement);
+            this.initTimer();
         }
+    }
+
+    initTimer = () => {
+        this.gameStarted = true;
+        $('<span/>', {
+            "id": 'game-timer',
+            "class": 'game-timer',
+            "text": this.time
+        }).appendTo(this.gameElement); // add timer span
+        let intervalthis = this;
+        timer = setInterval(function() {
+            intervalthis.time = --intervalthis.time;
+            $("#game-timer").text(intervalthis.time);
+            if(intervalthis.time == 0) {
+                intervalthis.gameStarted = false;
+                intervalthis.gameFinished = true;
+                clearInterval(timer);
+            }
+        }, 1000);
     }
     
 }
+
 
 let initBackground = function(gameElement) {
         $('<div/>', {
